7a947fb1173ddf489f76eabf4b2da3230c483cc2
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-06-02 11:01:39 +0200

    Main cleanup & cosmetic chanegs

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,39 @@
-/* 
-
-1. Projekt obiektowy:
-	- klasy
-	- konstruktory, destruktory
-- konstruktor kopiujący
-	- deklaracja przyjaciela
-	- dziedziczenie
-	- polimorfizm
-	- klasa abstrakcyjna
-	- czysto wirtualne metody
-
-2. STL
-- szablony
-- iteratory
-- algorytmy
-
-3. C++
-- zakresowa pętla for
-- własny szablon klasy (lub funkcji)
-- przeciążanie operatorów
-- wyjątki
-- plik .h z deklaracja i .cpp z definicją metod - linker
-
-*/
-
 #include <iostream>
 #include "Editor.h"
 #include "Event.h"
 #include "Snapshot.h"
 
-//#############################################################################
-
 int main() {
 
 	Editor e;
-	EditorSnapshot const* snap;
 
 	auto saveHandler = [](){
 		std::cout << "text saved!" << std::endl;
 	};
+	
 	auto restoreHandler = []() { 
 		std::cout << "text restored!" << std::endl;
 	};
+	
+	auto saveOrRestoreHandler = [](){
+		std::cout << "text saved or restored!" << std::endl;
+	};
+
 	auto editHandler = [](std::string text) {
 		std::cout << "text changed:\t" << text <<  std::endl;
 	};
 
-	connect(e.onSave, saveHandler);
-	connect(e.onRestore, restoreHandler);
-	connect(e.onEdit, editHandler);
+	e.onSave >> saveHandler;
+	e.onRestore >> restoreHandler;
+	e.onEdit >> editHandler;
+	connect(e.onSave | e.onRestore, saveOrRestoreHandler);
 
 	e.setText("Lorem ipsum");
-	snap = e.save();
-	e.setText("dolor sit amet");
-	e.restore(snap);
+	EditorSnapshot const* s = e.save();
+	e.setText("Dolor sit amet");
+	e.restore(s);
 
-	std::cout << "Text is:\t" << e.getText() << std::endl;
+	std::cout << "text is:\t" << e.getText() << std::endl;
 
 	return 0;
 }
